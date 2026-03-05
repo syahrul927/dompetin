70e15189ecddd58b7ad480aba763103877bcd7fc
Syahrul Ataufik <syahrulataufikaja11@gmail.com>
2026-03-05 11:47:18 +0700

    fix(pwa): change middleware protection exclude

--- src/middleware.ts
+++ src/middleware.ts
@@ -54,6 +54,8 @@ export default async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files
-    "/((?!_next|api|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|manifest)).*)",
+    "/((?!_next/static|_next/image|favicon.ico|manifest.json|sw.js|icons/|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Selalu jalankan untuk API routes
+    "/(api|trpc)(.*)",
   ],
 };
